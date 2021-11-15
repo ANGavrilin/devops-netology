c9784b7ecca72207f076e437c984272eba4b26bb
Aleksandr Gavrilin <gavrilin.post@gmail.com>
2021-11-15 16:58:23 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# display command line option
+# display command line options
 
 
 count=1
+<<<<<<< HEAD
 for param in "$@"; do
     echo "\$@ Parameter #$count = $param"
+
     count=$(( $count + 1))
 done
 
